d95e2c48fc7b2947a8f0edd4f63b7d5b17468f1a
Leif Neve <leifneve@gmail.com>
2024-07-10 18:05:24 -0400

    Update db_env.php
    Remove credentials.

--- include/db_env.php
+++ include/db_env.php
@@ -1,9 +1,9 @@
 <?php
 // Database environment everywhere.
-if (gethostname() == ("lhc-lx-stage.nlm.nih.gov" || "lhc-lx-stage-swap.nlm.nih.gov")) {
+if (gethostname() == "lhc-lx-stage.nlm.nih.gov") {
   $db_host = 'localhost';
-  $db_user = 'pubweb';
-  $db_pass = 'pub4proj@ebm';
+  $db_user = '';
+  $db_pass = '';
 } else {
   $db_host = getenv('MYSQL_HOST');
   $db_user = getenv('MYSQL_USER');
